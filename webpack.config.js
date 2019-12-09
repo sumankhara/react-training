fac4c01f43b1d257a78ffbbf7c393d01941fc451
Suman Khara <suman.khara@cerner.com>
2019-12-09 14:14:41 +0530

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
